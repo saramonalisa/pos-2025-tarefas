9fb012a9af0f8b921c6189c2625602ba61756d11
saramonalisa <162708657+saramonalisa@users.noreply.github.com>
2025-06-19 20:53:54 -0300

    Tarefa 09 - Parser cardápio xml

--- parsers/cardapio.py
+++ parsers/cardapio.py
@@ -5,22 +5,29 @@ dom = parse("parsers/cardapio.xml")
 cardapio = dom.documentElement
 
 pratos = cardapio.getElementsByTagName('prato')
- 
+
 print("Cardápio:")
 for prato in pratos:
     id = prato.getAttribute('id')
     nome = prato.getElementsByTagName('nome')[0].firstChild.nodeValue
-    descricao = prato.getElementsByTagName('descricao')[0].firstChild.nodeValue
-    ingredientes = prato.getElementsByTagName('ingredientes')[0].firstChild.nodeValue
-    preco = prato.getElementsByTagName('preco')[0].firstChild.nodeValue
-    calorias = prato.getElementsByTagName('calorias')[0].firstChild.nodeValue
-    tempoPreparo = prato.getElementsByTagName('tempoPreparo')[0].firstChild.nodeValue
-    
-    print(f"{prato.getAttribute('id')} - {prato}")
+    print(f"{id} - {nome}")
+
 id_lido = int(input("Digite o id do prato para saber mais: "))
-print(f"Nome: {nome}")
-print(f"Descrição: {descricao}")
-print(f"Ingredientes: {ingredientes}")
-print(f"Preço: {preco}")
-print(f"Calorias: {calorias}")
-print(f"Tempo de preparo: {tempoPreparo}")
+prato = pratos[id_lido-1]
+print("---\n")
+
+nome = prato.getElementsByTagName('nome')[0].firstChild.nodeValue
+descricao = prato.getElementsByTagName('descricao')[0].firstChild.nodeValue
+ingredientes = []
+for ingrediente in prato.getElementsByTagName('ingrediente'):
+    ingredientes.append(ingrediente.firstChild.nodeValue)
+preco = prato.getElementsByTagName('preco')[0].firstChild.nodeValue
+calorias = prato.getElementsByTagName('calorias')[0].firstChild.nodeValue
+tempoPreparo = prato.getElementsByTagName('tempoPreparo')[0].firstChild.nodeValue
+
+print("Nome:", nome)
+print("Descrição:", descricao)
+print("Ingredientes:", ", ".join(ingredientes))
+print("Preço:", preco)
+print("Calorias:", calorias)
+print("Tempo de Preparo:", tempoPreparo)
